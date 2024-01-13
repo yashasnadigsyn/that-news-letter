ce65e7c817783b03e6eb282926a33f1a7d2f16df
Yashas Nadig <103478177+yashasnadigsyn@users.noreply.github.com>
2024-01-13 23:39:55 +0530

    basic

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, World!");
+use actix_web::{web, App, Responder, HttpRequest, HttpServer};
+
+
+async fn greet(req: HttpRequest) -> impl Responder {
+    let name = req.match_info().get("name").unwrap_or("World");
+    format!("Hello {}!", &name)
+}
+#[tokio::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .route("/", web::get().to(greet))
+            .route("/{name}", web::get().to(greet))
+    })
+        .bind("127.0.0.1:9090")?
+        .run()
+        .await
 }
