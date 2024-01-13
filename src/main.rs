bcb7b6bd604fc6c9ee9b73e4c86510d3d5b86691
Yashas Nadig <103478177+yashasnadigsyn@users.noreply.github.com>
2024-01-14 00:12:53 +0530

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,20 @@
-use actix_web::{web, App, Responder, HttpRequest, HttpServer};
+use actix_web::{web, App, Responder, HttpRequest, HttpServer, HttpResponse};
 
 
 async fn greet(req: HttpRequest) -> impl Responder {
     let name = req.match_info().get("name").unwrap_or("World");
     format!("Hello {}!", &name)
 }
+
+async fn health_check() -> impl Responder {
+    HttpResponse::Ok()
+}
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new()
             .route("/", web::get().to(greet))
-            .route("/{name}", web::get().to(greet))
+            .route("/health_check", web::get().to(health_check))
     })
         .bind("127.0.0.1:9090")?
         .run()
