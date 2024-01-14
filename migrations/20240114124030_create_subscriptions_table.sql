f8df655d2102a2c117e7708a2bba0acd46b8ea1f
Yashas Nadig <103478177+yashasnadigsyn@users.noreply.github.com>
2024-01-14 18:38:41 +0530

    deleted init_Db.sh

--- migrations/20240114124030_create_subscriptions_table.sql
+++
@@ -1,9 +0,0 @@
--- Add migration script here
--- create subscriptions table
-CREATE TABLE subscriptions(
-    id uuid NOT NULL,
-    PRIMARY KEY (id),
-    email TEXT NOT NULL UNIQUE,
-    name TEXT NOT NULL,
-    subscribed_at timestamptz NOT NULL,
-)
