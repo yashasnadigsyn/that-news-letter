312bb3b206419c42a58a78e41e684c256081e7c2
Yashas Nadig <103478177+yashasnadigsyn@users.noreply.github.com>
2024-01-16 01:08:24 +0530

    Added telemetry

---
+++ src/telemetry.rs
@@ -0,0 +1,19 @@
+use tracing::Subscriber;
+use tracing::subscriber::set_global_default;
+use tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer};
+use tracing_subscriber::{layer::SubscriberExt, EnvFilter, Registry};
+use tracing_log::LogTracer;
+
+pub fn get_subscriber(name: String, env_filter: String) -> impl Subscriber + Send + Sync {
+    let env_filter = EnvFilter::try_from_default_env()
+        .unwrap_or_else(|_| EnvFilter::new(env_filter));
+    let formatting_layer = BunyanFormattingLayer::new(name, std::io::stdout);
+    Registry::default()
+        .with(env_filter)
+        .with(JsonStorageLayer)
+        .with(formatting_layer)
+}
+pub fn init_subscriber(subscriber: impl Subscriber + Sync + Send) {
+    LogTracer::init().expect("Failed initialize LogTracer!");
+    set_global_default(subscriber).expect("Cannot set global default");
+}
