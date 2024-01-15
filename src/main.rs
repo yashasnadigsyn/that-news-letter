93e52c03888eee7915449706fd25c6400d379552
Yashas Nadig <103478177+yashasnadigsyn@users.noreply.github.com>
2024-01-16 00:22:42 +0530

    removed log crate and added tracing crate

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,11 @@ use std::net::TcpListener;
 use the_news_letter::configuration::get_configuration;
 use the_news_letter::startup::run;
 use sqlx::{PgPool};
+use env_logger::Env;
+
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
+    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
     let configuration = get_configuration().expect("Cannot get configuration!");
     let connection_pool = PgPool::connect(&configuration.database.connection_string())
         .await
--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -2,6 +2,7 @@ use actix_web::{web, HttpResponse};
 use chrono::Utc;
 use sqlx::PgPool;
 use uuid::Uuid;
+use tracing::Instrument;
 
 #[derive(serde::Deserialize)]
 pub struct FormData {
@@ -10,6 +11,15 @@ pub struct FormData {
 }
 
 pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> HttpResponse {
+    let request_id = uuid::Uuid::new_v4();
+    let request_span = tracing::info_span!(
+        "Adding a new subscriber",
+        %request_id,
+        subscriber_email = %form.email,
+        subscriber_name = %form.name
+    );
+    let _request_span_guard = request_span.enter();
+    let query_span = tracing::info_span!("Saving new subscriber to the database");
     match sqlx::query!(
         r#"
     INSERT INTO subscriptions (id, email, name, subscribed_at)
@@ -21,10 +31,14 @@ pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> Ht
         Utc::now()
     )
         .execute(pool.as_ref())
+        .instrument(query_span)
         .await
     {
-        Ok(_) => HttpResponse::Ok().finish(),
+        Ok(_) => {
+            HttpResponse::Ok().finish()
+        }
         Err(e) => {
+            tracing::error!("REQUEST_ID: {}. Failed to add new subscriber to database: {:?}", request_id, e);
             println!("Failed to execute query: {}", e);
             HttpResponse::InternalServerError().finish()
         }
